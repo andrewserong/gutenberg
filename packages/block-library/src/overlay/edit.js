25b0ae8448394487c1401fc748eecf920fc54546
Andrew Serong <andrewserong@users.noreply.github.com>
2021-05-18 16:58:21 +1000

    Attempt to get overlay working within the editor canvas, and blocks editable

--- packages/block-library/src/overlay/edit.js
+++ packages/block-library/src/overlay/edit.js
@@ -9,21 +9,46 @@ import classnames from 'classnames';
 import { __ } from '@wordpress/i18n';
 import {
 	BlockControls,
+	InspectorControls,
 	RichText,
 	useBlockProps,
 	__experimentalUseInnerBlocksProps as useInnerBlocksProps,
 } from '@wordpress/block-editor';
+import { useSelect, useDispatch } from '@wordpress/data';
 import { close as closeIcon } from '@wordpress/icons';
-import { Button, ToolbarButton, ToolbarGroup } from '@wordpress/components';
+import { ToolbarButton, ToolbarGroup } from '@wordpress/components';
 import { useState } from '@wordpress/element';
+// import { store as editPostStore } from '@wordpress/edit-post';
+
+// import { store as editPostStore } from '../../store';
 
 export default function OverlayEdit( {
 	attributes,
 	isSelected,
 	setAttributes,
 } ) {
+	const { isInserterOpened, isEditorSidebarOpened } = useSelect(
+		( select ) => {
+			const isBlockInserterOpened = select(
+				'core/edit-post'
+			).isInserterOpened();
+			const isBlockEditorSidebarOpened = select(
+				'core/edit-post'
+			).isEditorSidebarOpened();
+			return {
+				isInserterOpened: isBlockInserterOpened,
+				isEditorSidebarOpened: isBlockEditorSidebarOpened,
+			};
+		}
+	);
+
 	const [ overlayContext, setOverlayContext ] = useState( 'button' );
-	const blockProps = useBlockProps();
+	const blockProps = useBlockProps( {
+		className: classnames( {
+			'wp-block-overlay--sidebar-opened': isEditorSidebarOpened,
+			'wp-block-overlay--inserter-opened': isInserterOpened,
+		} ),
+	} );
 
 	const { overlayButtonContent } = attributes;
 
@@ -75,13 +100,7 @@ export default function OverlayEdit( {
 						withoutInteractiveFormatting
 					/>
 				</div>
-				<div { ...innerBlocksProps }>
-					<Button
-						className="wp-block-overlay__close-button"
-						icon={ closeIcon }
-						onClick={ () => setOverlayContext( 'button' ) }
-					/>
-				</div>
+				<div { ...innerBlocksProps }></div>
 			</div>
 		</>
 	);
